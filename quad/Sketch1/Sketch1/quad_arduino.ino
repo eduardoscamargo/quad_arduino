8f5ba90cc8d6087789cdf3167940abdf0a04b0cc
Eduardo Camargo <eduardoscamargo@gmail.com>
2014-12-04 23:13:19 -0200

    Primeiro commit.

---
+++ quad/Sketch1/Sketch1/quad_arduino.ino
@@ -0,0 +1,20 @@
+/*
+ * Sketch1.ino
+ *
+ * Created: 12/4/2014 11:08:51 PM
+ * Author: eduardo
+ */ 
+
+void setup()
+{
+
+	  /* add setup code here, setup code runs once when the processor starts */
+
+}
+
+void loop()
+{
+
+	  /* add main program code here, this code starts again each time it ends */
+
+}
